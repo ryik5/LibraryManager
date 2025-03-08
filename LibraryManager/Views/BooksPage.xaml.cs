6aed49b216dc407854a0c85954c59136645007e7
yuri <ryik.yuri@gmail.com>
2025-03-08 17:15:26 +0200

    - deleted commented code in BooksPage.xaml.cs - deleted extra properties in BooksPage.xaml

--- LibraryManager/Views/BooksPage.xaml.cs
+++ LibraryManager/Views/BooksPage.xaml.cs
@@ -10,18 +10,18 @@ public partial class BooksPage : ContentPage
     public BooksPage()
     {
         InitializeComponent();
-        
+
         // Fetch the singleton instance of BooksViewModel
         BindingContext ??= App.Services.GetService<BooksViewModel>();
     }
-    
+
     /// <summary>
     /// To ensure the BindingContext is bound correctly
     /// </summary>
     protected override void OnAppearing()
     {
         base.OnAppearing();
-        
+
         // Avoid assigning a new instance unnecessarily
         BindingContext ??= App.Services.GetService<BooksViewModel>();
 
@@ -29,37 +29,32 @@ public partial class BooksPage : ContentPage
         BooksCollectionView.SelectionChanged += SelectableItemsView_OnSelectionChanged;
     }
 
-    // TODO : finish
-    /*Left only one way to select:
-           SelectedItems="{Binding SelectedBooks, Mode=TwoWay}"
-            SelectionChanged="SelectableItemsView_OnSelectionChanged">*/
-    
     /// <summary>
     /// To ensure the BindingContext is cleared correctly
     /// </summary>
     protected override void OnDisappearing()
     {
         // prevention of hung up switching between pages after it has been done selection of the row 
-        BooksCollectionView.SelectedItems=null;
-        
+        BooksCollectionView.SelectedItems = null;
+
         base.OnDisappearing();
 
-        if(BooksCollectionView !=null)
-            BooksCollectionView.SelectionChanged-= SelectableItemsView_OnSelectionChanged;
-        
+        if (BooksCollectionView != null)
+            BooksCollectionView.SelectionChanged -= SelectableItemsView_OnSelectionChanged;
+
         if (BindingContext != null)
             BindingContext = null;
     }
 
-    
-   /// <summary>
-   /// Event handler for SelectionChanged event
-   /// </summary>
+
+    /// <summary>
+    /// Event handler for SelectionChanged event
+    /// </summary>
     private void SelectableItemsView_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
     {
         if (BindingContext is BooksViewModel bvs)
         {
-            bvs.SelectedBooks.ResetAndAddRange(e.CurrentSelection.Select((b=>b as Book)));
+            bvs.SelectedBooks.ResetAndAddRange(e.CurrentSelection.Select((b => b as Book)));
         }
     }
 }
