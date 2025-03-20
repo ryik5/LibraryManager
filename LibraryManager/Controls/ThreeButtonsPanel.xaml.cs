2b1978dd2fef7c1ea878fff6208483be4330f1c3
yuri <ryik.yuri@gmail.com>
2025-03-20 17:03:22 +0200

    - changed button's style - added colors changed controls' colors

--- LibraryManager/Controls/ThreeButtonsPanel.xaml.cs
+++ LibraryManager/Controls/ThreeButtonsPanel.xaml.cs
@@ -8,20 +8,22 @@ public partial class ThreeButtonsPanel : ContentView
     {
         InitializeComponent();
     }
-    
+
 
     public static readonly BindableProperty NavigateCommandProperty = BindableProperty.Create(nameof(NavigateCommand),
         typeof(ICommand), typeof(ThreeButtonsPanel),
         defaultBindingMode: BindingMode.OneTime);
+
     public ICommand NavigateCommand
     {
         get => (ICommand)GetValue(NavigateCommandProperty);
         set => SetValue(NavigateCommandProperty, value);
     }
-    
-    
+
+
     public static readonly BindableProperty OkTextProperty = BindableProperty.Create(nameof(OkText), typeof(string),
         typeof(ThreeButtonsPanel), defaultBindingMode: BindingMode.OneTime);
+
     public string OkText
     {
         get => (string)GetValue(OkTextProperty);
@@ -31,6 +33,7 @@ public partial class ThreeButtonsPanel : ContentView
     public static readonly BindableProperty IsOkVisibileProperty = BindableProperty.Create(nameof(IsOkVisibile),
         typeof(bool), typeof(ThreeButtonsPanel),
         defaultBindingMode: BindingMode.OneTime);
+
     public bool IsOkVisibile
     {
         get => (bool)GetValue(IsOkVisibileProperty);
@@ -38,30 +41,31 @@ public partial class ThreeButtonsPanel : ContentView
     }
 
 
-
     public static readonly BindableProperty NoTextProperty = BindableProperty.Create(nameof(NoText), typeof(string),
         typeof(ThreeButtonsPanel),
         defaultBindingMode: BindingMode.OneTime);
+
     public string NoText
     {
         get => (string)GetValue(NoTextProperty);
         set => SetValue(NoTextProperty, value);
     }
-    
+
     public static readonly BindableProperty IsNoVisibileProperty = BindableProperty.Create(nameof(IsNoVisibile),
         typeof(bool), typeof(ThreeButtonsPanel),
         defaultBindingMode: BindingMode.OneTime);
+
     public bool IsNoVisibile
     {
         get => (bool)GetValue(IsNoVisibileProperty);
         set => SetValue(IsNoVisibileProperty, value);
     }
 
-    
 
     public static readonly BindableProperty CancelTextProperty = BindableProperty.Create(nameof(CancelText),
         typeof(string), typeof(ThreeButtonsPanel),
         defaultBindingMode: BindingMode.OneWay);
+
     public string CancelText
     {
         get => (string)GetValue(CancelTextProperty);
@@ -70,7 +74,8 @@ public partial class ThreeButtonsPanel : ContentView
 
     public static readonly BindableProperty IsCancelVisibileProperty = BindableProperty.Create(nameof(IsCancelVisibile),
         typeof(bool), typeof(ThreeButtonsPanel),
-        defaultBindingMode: BindingMode.OneWay);/*, defaultValueCreator: GetDefaultCancelVisibility*/
+        defaultBindingMode: BindingMode.OneWay); /*, defaultValueCreator: GetDefaultCancelVisibility*/
+
     public bool IsCancelVisibile
     {
         get => (bool)GetValue(IsCancelVisibileProperty);
@@ -79,5 +84,4 @@ public partial class ThreeButtonsPanel : ContentView
 
     /*private static object GetDefaultCancelVisibility(BindableObject bindable) =>
         ((ThreeButtonsPanel)bindable).IsCancelVisibile = true;*/
-    
 }
