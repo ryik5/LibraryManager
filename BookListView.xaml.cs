8955da21382cc2bd147f832e82677189599f5e3e
yuri <ryik.yuri@gmail.com>
2025-03-26 16:42:03 +0200

    - added tooltips - aligned book fields in the table - reordered book fields

---
+++ BookListView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LibraryManager.Views;
+
+/// <summary>
+/// Interaction logic for BookListView.xaml
+/// </summary>
+/// <author>YR 2025-01-14</author>
+public partial class BookListView : UserControl
+{
+    public BookListView()
+    {
+        InitializeComponent();
+    }
+}
