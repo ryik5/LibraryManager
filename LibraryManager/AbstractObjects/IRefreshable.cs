eb9626b2e6d29b325f74fcec95392ab6f4c101c6
yuri <ryik.yuri@gmail.com>
2025-04-09 21:25:39 +0300

    - added annotations

--- LibraryManager/AbstractObjects/IRefreshable.cs
+++ LibraryManager/AbstractObjects/IRefreshable.cs
@@ -1,6 +1,14 @@
 namespace LibraryManager.AbstractObjects;
 
+/// <summary>
+/// Represents an interface for objects that can refresh their controls when appearing.
+/// </summary>
+/// <author>YR 2025-03-09</author>
 public interface IRefreshable
 {
+    /// <summary>
+    /// Refreshes the controls when the object appears.
+    /// </summary>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task RefreshControlsOnAppearing();
 }
