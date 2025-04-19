77f997ed47d19051146b44ac187758a1e3b8ea07
yuri <ryik.yuri@gmail.com>
2025-04-19 14:52:55 +0300

    - minor changes

--- LibraryManager/MauiProgram.cs
+++ LibraryManager/MauiProgram.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Logging;
 
 namespace LibraryManager;
 
+/// <author>YR 2025-02-09</author>
 public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
--- LibraryManager/Platforms/MacCatalyst/Program.cs
+++ LibraryManager/Platforms/MacCatalyst/Program.cs
@@ -2,6 +2,7 @@
 
 namespace LibraryManager;
 
+/// <author>YR 2025-02-09</author>
 public class Program
 {
     // This is the main entry point of the application.
